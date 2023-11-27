f4ff676052746a7a8b00d6039f5a37aeeb2552bc
Nikita <nikita_rs7@ukr.net>
2023-11-28 01:32:01 +0200

    Update 2023-11-27T23:31:58.949Z

--- gulp/config/ftp.js
+++
@@ -1,6 +0,0 @@
-export const configFTP = {
-   host:"",
-   user:"",
-   password:"",
-   parallel:5 // Stream count
-}
--- gulp/tasks/ghpages.js
+++
@@ -1,5 +0,0 @@
-import ghPages from "gulp-gh-pages";
-
-export const ghPagesTask = () => {
-  return app.gulp.src(`./${app.path.buildFolder}/**/*`).pipe(ghPages());
-};
