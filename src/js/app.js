ebf243a7e945fd7cee5c544fd7b1e4438f60a326
Nikita <nikita_rs7@ukr.net>
2023-11-30 21:25:15 +0200

    'footer'

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,17 @@
 import { openMenu } from './features/burger.js';
 import {useDynamicAdapt} from './features/dynamicAdapt.js';
-import { smoothScrollTo } from './features/smoothAnchors.js';
-import { isTitleAnimated, sectionInterObserver } from './features/sectionObserver.js';
-import { swiper } from './features/swiper.js';
-import { initRatings } from './features/rating.js';
-import { spoilerInit } from './features/spoiler.js';
+// import { smoothScrollTo } from './features/smoothAnchors.js';
+// import { isTitleAnimated, sectionInterObserver } from './features/sectionObserver.js';
+// import { swiper } from './features/swiper.js';
+// import { initRatings } from './features/rating.js';
+// import { spoilerInit } from './features/spoiler.js';
 import * as ndevFunctions from './modules/functions.js';
 
 ndevFunctions.isWebp();
 
 
-// openMenu();
-// useDynamicAdapt();
+openMenu();
+useDynamicAdapt();
 // smoothScrollTo();
 // isTitleAnimated();
 // sectionInterObserver();
