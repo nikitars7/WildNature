5a4d7b8281833c4e2aae43d7fb37d0e5a891db75
Nikita <nikita_rs7@ukr.net>
2023-12-21 01:45:10 +0200

    'completed'

--- src/js/features/swiper.js
+++ src/js/features/swiper.js
@@ -1,16 +1,16 @@
 import Swiper from "swiper";
-import {Navigation,Controller,Parallax} from 'swiper/modules';
+import { Navigation, Controller, Parallax } from "swiper/modules";
 export const swiper = () => {
-  if (document.querySelector('.hero__slider')) {
-   const mainSlider = new Swiper('.hero__slider', {
-      modules: [Navigation,Controller,Parallax],
+  if (document.querySelector(".hero__slider")) {
+    const mainSlider = new Swiper(".hero__slider", {
+      modules: [Navigation, Controller, Parallax],
       observer: true,
       observeParents: true,
       slidesPerView: 1,
-      parallax:true,
+      parallax: true,
       spaceBetween: 30,
-      loop:true,
-      centeredSlides:true,
+      loop: true,
+      centeredSlides: true,
       // autoHeight: true,
       speed: 800,
       // pagination: {
@@ -18,80 +18,82 @@ export const swiper = () => {
       //   clickable: true,
       // },
       navigation: {
-        prevEl: '.hero__arrow--left',
-        nextEl: '.hero__arrow--right',
+        prevEl: ".hero__arrow--left",
+        nextEl: ".hero__arrow--right",
       },
-      on:{
-        init:(slider)=>{
-          slider.slides.forEach(slide => {
-            const imageSrc = slide.querySelector('.slide-hero__image').getAttribute('src');
+      on: {
+        init: (slider) => {
+          slider.slides.forEach((slide) => {
+            const imageSrc = slide
+              .querySelector(".slide-hero__image")
+              .getAttribute("src");
             const topImage = `
             <div class="slide-hero__top-image">
             <img src=${imageSrc} alt="sliderimg">
           </div>
             `;
-            const slideContent = slide.querySelector('.slide-hero__content');
-            slideContent.insertAdjacentHTML('beforeend',topImage)
-          })
-        }
-      }
-    })
-  //  const miniSwiper =  new Swiper('.hero__mini-slider', {
-  //     modules: [Navigation,Controller],
-  //     observer: true,
-  //     observeParents: true,
-  //     slidesPerView: 1,
-  //     spaceBetween: 20,
-  //     slideToClickedSlide:true,
-  //     loop:true,
-  //     loopAdditionalSlides:10,
-  //     centeredSlides:true,
-  //     // autoHeight: true,
-  //     speed: 800,
-  //     // pagination: {
-  //     //   el: '.reviews__bullets',
-  //     //   clickable: true,
-  //     // },
-  //     // navigation: {
-  //     //   prevEl: '.hero__arrow--left',
-  //     //   nextEl: '.hero__arrow--right',
-  //     // },
-  //     on:{
-  //       init:(slider)=>{
-  //         slider.slides.forEach(slide => {
-  //           const imageSrc = slide.querySelector('.slide-hero__image').getAttribute('src');
-  //           const topImage = `
-  //           <div class="slide-hero__top-image">
-  //           <img src=${imageSrc} alt="sliderimg">
-  //         </div>
-  //           `
-  //           slide.insertAdjacentHTML('beforeend',topImage)
-  //         })
-  //       }
-  //     }
-  //   })
+            const slideContent = slide.querySelector(".slide-hero__content");
+            slideContent.insertAdjacentHTML("beforeend", topImage);
+          });
+        },
+      },
+    });
+    //  const miniSwiper =  new Swiper('.hero__mini-slider', {
+    //     modules: [Navigation,Controller],
+    //     observer: true,
+    //     observeParents: true,
+    //     slidesPerView: 1,
+    //     spaceBetween: 20,
+    //     slideToClickedSlide:true,
+    //     loop:true,
+    //     loopAdditionalSlides:10,
+    //     centeredSlides:true,
+    //     // autoHeight: true,
+    //     speed: 800,
+    //     // pagination: {
+    //     //   el: '.reviews__bullets',
+    //     //   clickable: true,
+    //     // },
+    //     // navigation: {
+    //     //   prevEl: '.hero__arrow--left',
+    //     //   nextEl: '.hero__arrow--right',
+    //     // },
+    //     on:{
+    //       init:(slider)=>{
+    //         slider.slides.forEach(slide => {
+    //           const imageSrc = slide.querySelector('.slide-hero__image').getAttribute('src');
+    //           const topImage = `
+    //           <div class="slide-hero__top-image">
+    //           <img src=${imageSrc} alt="sliderimg">
+    //         </div>
+    //           `
+    //           slide.insertAdjacentHTML('beforeend',topImage)
+    //         })
+    //       }
+    //     }
+    //   })
     // mainSlider.controller.control = miniSwiper; // привязка слайдеров
     // miniSwiper.controller.control = mainSlider;
   }
-  const quoteSlider =  new Swiper('.quote-contacts__slider', {
+  const quoteSlider = new Swiper(".reviews__slider", {
     modules: [Navigation],
     observer: true,
     observeParents: true,
     slidesPerView: 1,
     spaceBetween: 20,
-    slideToClickedSlide:true,
+    slideToClickedSlide: true,
     // loop:true,
     // loopAdditionalSlides:10,
-    centeredSlides:true,
-    // autoHeight: true,
+    centeredSlides: true,
+    autoHeight: true,
     speed: 800,
     // pagination: {
     //   el: '.reviews__bullets',
     //   clickable: true,
     // },
     navigation: {
-      prevEl: '.quote__arrow--left',
-      nextEl: '.quote__arrow--right',
+      prevEl: ".reviews__arrow--left",
+      nextEl: ".reviews__arrow--right",
     },
-  })
-}
+  });
+};
